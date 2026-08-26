438a4164898d0898218b93b56d7e3df4bae855ec
OmadHalim <omadjonzokirjonov977@gmail.com>
2026-08-26 12:26:50 +0900

    BBR: building express web server

--- server.js
+++ server.js
@@ -15,12 +15,12 @@ app.set("views","views");
 app.set("view engine","ejs");
 
 // 4: Routing code
-app.get("/hello", function (req, res) {
-  res.end(`<h1 >HELLO WORLD</h1>` );
+app.post("/create-item",(req,res) => {
+  console.log(req.body);
+  res.json({test: "success"});
 });
-
-app.get("/gift", function (req, res) {
-  res.end(`<h1 >Siz sovg'alar bo'limidasiz</h1>` );
+app.get("/",function (req,res ) {
+  res.render("harid");
 });
 
 const server = http.createServer(app);
