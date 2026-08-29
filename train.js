64471b8e58c00272314268669e190efdc3e10000
OmadHalim <omadjonzokirjonov977@gmail.com>
2026-08-30 07:55:07 +0900

    add A task

--- train.js
+++ train.js
@@ -1,3 +1,21 @@
+// Task-A 
+
+function countLetter (letter, word) {
+  let count = 0;
+  for (let a = 0; a <word.length; a++) {
+    if (word[a] === letter) {
+      count++;
+    }
+  }
+  return count;
+}
+
+console.log(countLetter("a",
+  "alibaba"
+));
+
+
+
 // console.log("Jack Ma maslahatlari");
 // const list = [
 //   "yaxshi talaba bo'ling", // 0-20
@@ -33,54 +51,54 @@
 
 /// =-============================2-QISM=====================================================================//
 
-console.log("Jack Ma maslahatlari");
-const list = [
-  "yaxshi talaba bo'ling", // 0-20
-  "to'g'ri boshliq tanlang va ko'proq xato qiling", // 20-30
-  "o'zingizga ishlashingizni boshlang", // 30-40
-  "siz kuchli bo'lgan narsalarni qiling", // 50-60
-  "yoshlarga investitsiya qiling", // 50-60
-  "endi dam oling, foydasi yo'q endi", // 60
-];
+// console.log("Jack Ma maslahatlari");
+// const list = [
+//   "yaxshi talaba bo'ling", // 0-20
+//   "to'g'ri boshliq tanlang va ko'proq xato qiling", // 20-30
+//   "o'zingizga ishlashingizni boshlang", // 30-40
+//   "siz kuchli bo'lgan narsalarni qiling", // 50-60
+//   "yoshlarga investitsiya qiling", // 50-60
+//   "endi dam oling, foydasi yo'q endi", // 60
+// ];
 
 
- async function maslahatBering(a) {
-  if (typeof a !== "number") throw new Error ("insert a number");
-  else if (a <= 20) return list[0];
- else if (a> 20 && a <=30) return list[1];
- else if (a> 30 && a <=40) return list[2];
- else if (a> 40 && a <=50) return list[3];
- else if (a> 50 && a <=60) return list[4];
- else {
-     return new Promise((resolve,reject) => {
-      setTimeout(() => {
-        resolve(list[5]);
+//  async function maslahatBering(a) {
+//   if (typeof a !== "number") throw new Error ("insert a number");
+//   else if (a <= 20) return list[0];
+//  else if (a> 20 && a <=30) return list[1];
+//  else if (a> 30 && a <=40) return list[2];
+//  else if (a> 40 && a <=50) return list[3];
+//  else if (a> 50 && a <=60) return list[4];
+//  else {
+//      return new Promise((resolve,reject) => {
+//       setTimeout(() => {
+//         resolve(list[5]);
 
-      },5000);
-     });
+//       },5000);
+//      });
   
-  setTimeout(function () {
-    return list[5];
-  }, 5000);
- }
+//   setTimeout(function () {
+//     return list[5];
+//   }, 5000);
+//  }
   
-} 
- then/catch
-console.log("passed here 0");
-maslahatBering(20).then(data => {
-  console.log('javob:',data);
-}).catch(err => {
-console.log("ERROR:, err");
-})
-console.log("passed here 1");
+// } 
+//  then/catch
+// console.log("passed here 0");
+// maslahatBering(20).then(data => {
+//   console.log('javob:',data);
+// }).catch(err => {
+// console.log("ERROR:, err");
+// })
+// console.log("passed here 1");
 
-asyn/await
-async function run () {
-  let javob = await maslahatBering(20);
-  console.log(javob);
-  javob = await maslahatBering(70);
-  console.log(javob);
-  javob = await maslahatBering(41);
-  console.log(javob);
-}
-run();
+// asyn/await
+// async function run () {
+//   let javob = await maslahatBering(20);
+//   console.log(javob);
+//   javob = await maslahatBering(70);
+//   console.log(javob);
+//   javob = await maslahatBering(41);
+//   console.log(javob);
+// }
+// run();
