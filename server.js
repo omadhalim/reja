f633369bcb8f091fc8ba181911740befb927ba50
OmadHalim <omadjonzokirjonov977@gmail.com>
2026-09-02 03:39:21 +0900

    feat: mongoDB ulanish logic

--- server.js
+++ server.js
@@ -1,46 +1,34 @@
-console.log("Web Serverni boshlash");
-const express = require("express");
-const app = express();
 const http = require("http");
-const fs = require("fs");
+const mongodb = require("mongodb");
 
-let user;
-fs.readFile("database/user.json","utf8", (err, data) => {
-  if(err) {
-    console.log("ERROR:", err);
-  } else {
-    user = JSON.parse(data)
-  }
-});
+let db;
+const connectionString ="mongodb+srv://omadjonzokirjonov977_db_user:HM4_LBzpRUr9RC8@cluster0.yxils8n.mongodb.net/Reja"
+mongodb.connect(
+connectionString,
 
-// 1: Kirish code
-app.use(express.static("public"));
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+{ 
+  useNewUrlParser:true,
+  useUnifiedTopology: true,
 
-// 2: Session code
-
-// 3: Views code
-app.set("views","views");
-app.set("view engine","ejs");
-
-// 4: Routing code
-app.post("/create-item",(req,res) => {
-  console.log(req.body);
-  res.json({test: "success"});
-});
-app.get("/",function (req,res ) {
-  res.render("reja");
-});
-
-app.get('/author', (req,res) => {
-  res.render("author", { user: user});
-})
-
-const server = http.createServer(app); 
+  },
+  (err,client) =>{
+    if (err) {
+  console.log("ERROR on connection MongoDB");
+  console.log(err);
+} else {
+      console.log("MongoDB connection succeed");
+      module.exports = client;
+      console.log(client);
+      const app = require("./app");
+      const server = http.createServer(app); 
 let PORT = 3000;
 server.listen(PORT, function () {
   console.log(
     `The server is running successfully on port: ${PORT}, http://localhost:${PORT}`
   );
 });
+   
+    }
+  }
+);
+
