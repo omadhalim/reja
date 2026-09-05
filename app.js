5278d633791130690fbae90a2b83b6ac8755e2fe
OmadHalim <omadjonzokirjonov977@gmail.com>
2026-09-06 08:41:09 +0900

    add delete function

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
 console.log("Web Serverni boshlash");
 const express = require("express");
+const res = require("express/lib/response");
 const app = express();
-
-const fs = require("fs");
-
-let user;
-fs.readFile("database/user.json","utf8", (err, data) => {
-  if(err) {
-    console.log("ERROR:", err);
-  } else {
-    user = JSON.parse(data)
-  }
-});
 
 // MongoDB chaqirish
 const db = require("./server").db();
+const mongodb = require("mongodb");
+
+// const fs = require("fs");
+
+// let user;
+// fs.readFile("database/user.json","utf8", (err, data) => {
+//   if(err) {
+//     console.log("ERROR:", err);
+//   } else {
+//     user = JSON.parse(data)
+//   }
+// });
+
+// // MongoDB chaqirish
+// const db = require("./server").db();
+// const mongodb = require("mongodb");
 
 // 1: Kirish code
 app.use(express.static("public"));
@@ -36,7 +42,16 @@ app.post("/create-item",(req,res) => {
     console.log(data.ops);
     res.json(data.ops[0]);
   });
+});
   
+  app.post("/delete-item", (req, res) => {
+    const id = req.body.id;
+    db.collection("plans").deleteOne({_id:new mongodb.ObjectId(id)},
+  function (err,data) {
+    res.json({state: "success"});
+  }
+
+  );
 
 });
 app.get("/", (req, res) => {
--- public/browser.js
+++ public/browser.js
@@ -29,3 +29,24 @@ document
  });
 });
   
+document.addEventListener("click", function(e){
+   // delete oper 
+  if (e.target.classList.contains("delete-me")) {
+    if (confirm("Aniq ochirmoqchimisiz?")) {
+      axios
+      .post("/delete-item",{id: e.target.getAttribute("data-id")}) 
+      .then((response) => {
+        console.log(response.data);
+        e.target.parentElement.parentElement.remove();
+      })
+      .catch((err) => {
+        console.log("Iltimos qaytadan harakat qiling");
+      });
+    }
+  }
+
+  // edit oper
+  if (e.target.classList.contains("edit-me")) {
+    alert("siz edit tugmasini bosdingiz");
+  }
+});
