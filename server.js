4d857f4281d860c501b542818e159d2566b962ca
omadhalim <omadjonzokirjonov977@gmail.com>
2026-09-16 14:04:49 +0900

    deploy: Reja project

--- server.js
+++ server.js
@@ -21,8 +21,8 @@ connectionString,
       console.log(client);
       const app = require("./app");
       const server = http.createServer(app); 
-let PORT = 3000;
-server.listen(PORT, function () {
+const PORT = process.env.PORT || 7012;
+server.listen(PORT, "0.0.0.0", function () {
   console.log(
     `The server is running successfully on port: ${PORT}, http://localhost:${PORT}`
   );
